cc3cfca04d94e2ed506327f9304a3a0655a6ac8c
Sohail-Ansari1 <158168583+Sohail-Ansari1@users.noreply.github.com>
2024-01-30 20:24:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function videoAnimation(){
+    let play = document.querySelector("#scroll")
+let videocon = document.querySelector("#video-container")
+videocon.addEventListener("mouseenter",function(){
+ gsap.to(play,{
+
+opacity:1,
+scale:1
+
+ })
+
+
+})
+videocon.addEventListener("mouseleave",function(){
+    gsap.to(play,{
+   
+   opacity:0,
+   scale:0
+   
+    })
+   
+   
+   })
+   videocon.addEventListener("mousemove",function(dets){
+    gsap.to(play,{
+   
+  left:dets.x-50,
+  top:dets.y-50
+   
+    })
+   
+   
+   })
+
+}
+videoAnimation()
+
+let facebook = document.querySelector("#Sfacebook")
+
+
